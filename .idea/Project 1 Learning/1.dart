4752d3ad630d640c85f906175fb6ead8d83dc3ac
Raghib Ishraq Rahat <ishraq25503@gmail.com>
2025-07-15 11:58:29 +0600

    first commit

---
+++ .idea/Project 1 Learning/1.dart
@@ -0,0 +1,6 @@
+main(){
+  print ('Hello world');
+ } //git add .
+// git commit -m "Initial commit"
+// git branch -M main
+// git push origin main git config --global user.email "ishraq25503@gmail.com"
